2197e82439f4e406c3bad5a2b3fa095d3a0d7cb9
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2021-11-11 14:41:11 -0800

    Update drift_spider.py

--- drift_scrapy_project/spiders/drift_spider.py
+++ drift_scrapy_project/spiders/drift_spider.py
@@ -1,30 +1,43 @@
 import scrapy
 import json
+import time
+import datetime
 from os import makedirs
 from os.path import exists, dirname
 from scrapy_selenium import SeleniumRequest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+from drift_scrapy_project.items import DriftAccountList, DriftAccountUserList, DriftUserActivity
+
 class DriftSpider(scrapy.Spider):
     name = 'driftSpider'
     allowed_domains = ['drift.com']
     _cookies = ''
     bearerAuthToken = ''
-    outputFolder = './json_output/'
-    # Date ranges to scrape with API calls
-    fromTime = '1626246000000'
-    toTime = '1634281199999'
-    # Create output folder if it doesn't already exist.
-    makedirs(dirname(outputFolder), exist_ok=True)
+    userCount = 0
+    companyCount = 0
 
-    # You'll need to get the filter key for "All accounts" on your drift account and replace the URL below
-    # TODO: Waiting for the page title is probably not the best way to identify the page to start scraping
-    #       Probably better to just start at the login page and then auto-redirect to this page so user
-    #       doesn't have to click around.
+    custom_settings = {
+        'START_DATE': '10/1/2021',
+        'END_DATE': '10/1/2021'
+    }
+
+    def __init__(self, startDate="", endDate="", *args, **kwargs):
+        super(DriftSpider, self).__init__(*args, **kwargs)
+        # Date ranges to scrape with API calls
+        if startDate != "":
+            self.custom_settings['START_DATE'] = startDate
+        if endDate != "":
+            self.custom_settings['END_DATE']  = endDate
+        #makes a timestamp at the beginning of the beginning day
+        self.fromTimeStamp = int(time.mktime(datetime.datetime.strptime(self.custom_settings['START_DATE'], "%m/%d/%Y").timetuple())) * 1000
+        #makes a timestamp at the very end of the last day
+        self.toTimeStamp = int(time.mktime(datetime.datetime.combine(datetime.datetime.strptime(self.custom_settings['END_DATE'], "%m/%d/%Y"), datetime.time.max).timetuple())) * 1000
+
     def start_requests(self):
         yield SeleniumRequest(
-            url='https://app.drift.com/conversations/prospector/accounts?filters=eyJ2ZXJzaW9uIjoyLCJmaWx0ZXJHcm91cHNCeVBhZ2UiOnsiQUNDT1VOVFMiOltdLCJBQ0NPVU5UX0RFVEFJTFMiOltdLCJDT05UQUNUX0RFVEFJTFMiOltdLCJBQ1RJVklUSUVTIjpbW11dfX0&savedFilterId=0',
+            url='https://app.drift.com/conversations/prospector/activity',
             #url="https://drift.com",
             wait_time=120,
             wait_until=EC.title_is('Drift | Account Activity'),
@@ -38,33 +51,33 @@ class DriftSpider(scrapy.Spider):
         self._cookies = response.request.meta['driver'].get_cookies()
         authToken = response.request.meta['driver'].get_cookie("DrifttAuth")
         self.bearerAuthToken = authToken["value"]
-
         # TODO: Move the fromTime and toTime to be parameterized so we don't rescrap ALL records in future
         yield scrapy.Request(
             url="https://account-activity.drift.com/v2/accounts/search_with_activities/?pageSize=500",
             callback=self.parse_account_list,
             method='POST',
             body='''{
-                "sortBy":"ENGAGEMENT_SCORE",
-                "descending":"true",
+                "sortBy":"ACCOUNT_NAME",
+                "descending":"false",
                 "accountName":"",
-                "fromTime":'''+ self.fromTime + ''',
-                "toTime":''' + self.toTime + ''',
+                "fromTime":'''+ str(self.fromTimeStamp) + ''',
+                "toTime":''' + str(self.toTimeStamp) + ''',
                 "filterGroups":[]
             }''',
             headers={
-                "accept": "application/json, text/plain, */*",
                 "content-type": "application/json",
                 "authorization": "Bearer " + self.bearerAuthToken
             })
 
     def parse_account_list(self, response):
-        accounts_file = open(self.outputFolder + 'accounts.json', 'w')
-        n = accounts_file.write(response.text)
-        accounts_file.close()
-        accounts_json = json.loads(response.text)
-        for account in accounts_json['data']:           
-            #print(response.body)
+        accounts = json.loads(response.text)
+        accountItem = DriftAccountList(driftAccountList = accounts)
+        self.logger.info("Scraped all accounts")
+        yield accountItem
+        for account in accounts['data']:           
+            # Use this to not pull Magic Leap account (employee) activity
+            # if (account["accountId"] != "5002912_magicleap.com"):
+
             # Request POST - same headers as POST above
             # https://account-activity.drift.com/v2/accounts/xxxxxxx_xxxxxxxxxxxx/contacts
             yield scrapy.Request(
@@ -78,52 +91,39 @@ class DriftSpider(scrapy.Spider):
                     "contactFilterType":"ALL"
                     }''',
                 headers={
-                    "accept": "application/json, text/plain, */*",
                     "content-type": "application/json",
                     "authorization": "Bearer " + self.bearerAuthToken
                 },
                 cb_kwargs=dict(accountID=account["accountId"])
             )
-        # For each account, get the accountID and iterate the loop
-        # for account in accounts_json:
-
-
 
     def parse_account_contacts(self, response, accountID):
-        contacts_file = open(self.outputFolder + 'account_'+accountID+'_contacts.json', 'w')
-        n = contacts_file.write(response.text)
-        contacts_file.close()
-        contacts_json = json.loads(response.text)
-        print(contacts_json)
-        for contact in contacts_json:
+        account_users_response_json = json.loads(response.text)
+        account_user_item = DriftAccountUserList(driftAccountUserListJSON = account_users_response_json)
+        self.companyCount = self.companyCount + 1
+        self.logger.info("Scraped Company " + accountID + " - #" + str(self.companyCount))
+        yield account_user_item
+
+        for contact in account_users_response_json:
         # Request GET - same headers as POST
         # https://customer.api.drift.com/end_users/xxxxxxxxxxx
-            yield scrapy.Request(url="https://customer.api.drift.com/end_users/"+str(contact["endUserId"]),
+            yield scrapy.Request(url="https://customer.api.drift.com/end_users/"+str(contact["endUserId"])+"/timeline?size=999&offset=0&resolveMaster=false&filter=EVENT",
                 callback=self.parse_end_user,
                 method='GET',
                 headers={
-                    "accept": "application/json, text/plain, */*",
-                    "content-type": "application/json",
                     "authorization": "Bearer " + self.bearerAuthToken
-                }
+                },
+                cb_kwargs=dict(accountID=accountID, endUserID=str(contact["endUserId"]))
             )
+        
 
-    def parse_end_user(self, response):
+    def parse_end_user(self, response, accountID, endUserID):
         end_user_response_json = json.loads(response.text)
-        end_user_output_file = self.outputFolder + "end_users.json"
-        if not exists(end_user_output_file):
-            new_end_user_file = open(end_user_output_file, 'w')
-            json.dump([end_user_response_json], new_end_user_file, indent = 4)
-            new_end_user_file.close()
-        else:
-            with open(end_user_output_file,'r+') as file:
-                # First we load existing data into a dict.
-                file_data = json.load(file)
-                # Join new_data with file_data inside emp_details
-                file_data.append(end_user_response_json)
-                # Sets file's current position at offset.
-                file.seek(0)
-                # convert back to json.
-                json.dump(file_data, file, indent = 4)
+        end_user_item = DriftUserActivity(driftUserActivityJSON = end_user_response_json)
+        self.userCount = self.userCount + 1
+        reportOut = "Scraped EndUser " + endUserID + " from Company " + accountID + " - #" + str(self.userCount)
+        self.logger.info(reportOut)
+        print(reportOut)
+        return end_user_item
 
   
